67a8655cd79f35b19bca7f1b1463d5800f3ff11f
Karolina Tech Dev <101413311+KarolinaTechDev@users.noreply.github.com>
2022-04-09 14:43:30 -0400

    Function 3 - multiplying two numbers

---
+++ Function 3 - multiplying two numbers.js
@@ -0,0 +1,7 @@
+//Implement a function which multiplies two numbers.
+
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+let resulMultiply = multiply(4, 5);
+console.log(resulMultiply);
